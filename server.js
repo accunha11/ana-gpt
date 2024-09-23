bd24d7d8fc6efeddbffce983d1ff6796835e0f42
accunha11 <anacrcunha00@gmail.com>
2024-09-23 17:26:35 +0100

    set up input

--- server.js
+++ server.js
@@ -6,6 +6,35 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+// temporary key for set up, will have a new key later that is safely stored
+const API_KEY =
+	'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';
+
+app.post('/completions', async (req, res) => {
+	const options = {
+		method: 'POST',
+		headers: {
+			Authorization: `Bearer ${API_KEY}`,
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({
+			model: 'gpt-3.5-turbo',
+			messages: [{ role: 'user', content: req.body.message }],
+			max_tokens: 100,
+		}),
+	};
+	try {
+		const response = await fetch(
+			'https://api.openai.com/v1/chat/completions',
+			options
+		);
+		const data = await response.json();
+		res.send(data);
+	} catch (error) {
+		console.error(error);
+	}
+});
+
 app.listen(PORT, () =>
 	console.log('Your server is running on PORT ' + PORT)
 );
--- src/App.js
+++ src/App.js
@@ -1,4 +1,31 @@
+import { useState, useEffect } from 'react';
+
 const App = () => {
+	const [value, setValue] = useState('');
+	const [message, setMessage] = useState(null);
+
+	const getMessages = async () => {
+		const options = {
+			method: 'POST',
+			body: JSON.stringify({
+				message: value,
+			}),
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		};
+		try {
+			const response = await fetch(
+				'http://localhost:8000/completions',
+				options
+			);
+			const data = await response.json();
+			setMessage(data.choices[0].message);
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
 	return (
 		<div className='app'>
 			<section className='side-bar'>
@@ -15,8 +42,15 @@ const App = () => {
 				<ul className='feed'></ul>
 				<div className='bottom-section'>
 					<div className='input-container'>
-						<input />
-						<div id='submit'>➤</div>
+						<input
+							value={value}
+							onChange={(e) => setValue(e.target.value)}
+						/>
+						<div
+							id='submit'
+							onClick={getMessages}>
+							➤
+						</div>
 					</div>
 					<p className='info'>
 						ChatGPT can make mistakes. Check important
