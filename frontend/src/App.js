616790b298b79b1ff2591706e7d84152d99e6885
accunha11 <anacrcunha00@gmail.com>
2024-09-23 16:38:19 +0100

    side bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,23 @@
 const App = () => {
-	return <div className='app'></div>;
+	return (
+		<div className='app'>
+			<section className='side-bar'>
+				<button>+ New Chat</button>
+				<ul className='history'>
+					<li>BLAH</li>
+				</ul>
+				<nav>
+					<p>Made by Ana</p>
+				</nav>
+			</section>
+			<section className='main'>
+				<h1>AnaGPT</h1>
+				<ul className="feed">
+					
+				</ul>
+			</section>
+		</div>
+	);
 };
 
 export default App;
